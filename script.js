5c2f017ce70548fdadd64483878c7b452fc9a342
jxdoyle <56358030+jxdoyle@users.noreply.github.com>
2021-11-10 22:49:31 +0000

    added web server and css

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-// initialise variables
-const nightmare = require('nightmare')()
-
-// const Nightmare = require('nightmare')
-// const nightmare = Nightmare()
-
-// I think I may need to add something to allow "require" to work in browser outside of node.js in terminal
-
-// nightmare js does seem to be a bit outdated also tried puppeteer js but same issues arose
 // nightmare - https://github.com/segmentio/nightmare
-// puppeteer - https://github.com/puppeteer/puppeteer
+const Nightmare = require('nightmare')
+const nightmare = Nightmare()
 
 
-// call for script testing console logs should return ("node script.js" in terminal)
+// call for script testing console logs should return 
 // November and https://video.playphrase.me/5b1837c28079eb4cd4a53e99/5e8b7c0424aa9a0025987688.mp4
-playMonth()
+// playMonth()
 
 
-// on call runs search for current day of the week then passes the month to search function
+// on call runs search for current day of the week then passes the day to search function
 function playDay() {
     var currentDate = new Date()
     var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    document.getElementById("text-box").innerText = (dayNames[currentDate.getDay()])
 
     searchVideo(dayNames[currentDate.getDay()])
     console.log(dayNames[currentDate.getDay()])
@@ -29,6 +22,7 @@ function playDay() {
 function playMonth() {
     var currentDate = new Date();
     var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    document.getElementById("text-box").innerText = (monthNames[currentDate.getMonth()])
 
     searchVideo(monthNames[currentDate.getMonth()])
     console.log(monthNames[currentDate.getMonth()])
@@ -37,12 +31,11 @@ function playMonth() {
 // searches for the correct video based on the passed phrase and changes the iframe src appropriately 
 async function searchVideo(phrase) {
     var videoLink = await nightmare.goto("https://www.playphrase.me/#/search?q=" + phrase)
-                                    .wait("#video-player-0")
-                                    .evaluate(() => document.getElementById("video-player-0").getAttribute("src"))
-                                    .end()
+                                   .wait("#video-player-0")
+                                   .evaluate(() => document.getElementById("video-player-0").getAttribute("src"))
+                                   .end()
 
     // get id of iframe from index and change the src to the src found above
     // document.getElementById("video-player").setAttribute("src", videoLink)
-
     console.log(videoLink)
 }
