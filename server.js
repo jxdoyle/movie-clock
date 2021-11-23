3a11a7ee0928122ee8270018eb93fa979bb7ed09
jxdoyle <56358030+jxdoyle@users.noreply.github.com>
2021-11-23 03:35:48 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const socketIo = require('socket.io')
 const path = require('path')
 const cors = require('cors')
-const port = 3000
+const port = process.env.PORT || 3000
 
 const nightmare = require('./nightmare')
 
